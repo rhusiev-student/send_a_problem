541c7428e0aa391b51706606f3540eb0c059b132
rad1an <rad1an.pm@proton.me>
2022-11-08 12:30:37 +0200

    Create blank functions

---
+++ combinatorics.py
@@ -0,0 +1,119 @@
+"""
+Find some combinatorics functions
+"""
+
+
+def factorial(number: int) -> int:
+    """
+    Return factorial of a number
+
+    Parameters
+    ----------
+    number : int
+        number to calculate factorial
+
+    Returns
+    -------
+    int
+        factorial of the number
+
+    """
+    pass
+
+
+def combination(n: int, r: int) -> int:
+    """
+    Return combination of n and r
+
+    Parameters
+    ----------
+    n : int
+        number of elements
+    r : int
+        number of elements to choose
+
+    Returns
+    -------
+    int
+        number of combinations of n and r
+
+    """
+    pass
+
+
+def permutation(n: int) -> int:
+    """
+    Return permutation of n
+
+    Parameters
+    ----------
+    n : int
+        number of elements
+
+    Returns
+    -------
+    int
+        number of permutations of n
+
+    """
+    pass
+
+
+def variation(n: int, r: int) -> int:
+    """
+    Return variation of n and r
+
+    Parameters
+    ----------
+    n : int
+        number of elements
+    r : int
+        number of elements to choose
+
+    Returns
+    -------
+    int
+        number of variations of n and r
+
+    """
+    pass
+
+
+def combination_with_repetition(n: int, r: int) -> int:
+    """
+    Return combination with repetition of n and r
+
+    Parameters
+    ----------
+    n : int
+        number of elements
+    r : int
+        number of elements to choose
+
+    Returns
+    -------
+    int
+        number of combinations with repetition of n and r
+
+    """
+    pass
+
+
+def variation_with_repetition(n: int, r: int) -> int:
+    """
+    Return variation with repetition of n and r
+
+    Parameters
+    ----------
+    n : int
+        number of elements
+    r : int
+        number of elements to choose
+
+    Returns
+    -------
+    int
+        number of variations with repetition of n and r
+
+    """
+    pass
